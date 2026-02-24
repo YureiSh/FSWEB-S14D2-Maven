bdc199d729abd13af3fb0d99b2a84403b82baaaf
YureiSh <enisataerkol@hotmail.com>
2026-02-24 20:41:01 +0300

    18 tests passed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Bedroom bedroom = new Bedroom("test",
+                new Wall("South"),
+                new Wall("North"),
+                new Wall("East"),
+                new Wall("west"),
+                new Ceiling(10, PaintColor.RED),
+                new Bed("Lame",1,1,1,1),
+                new Lamp(LampType.LAVA, true, 10),
+                new Wardrobe(12,10,20.0),
+                new Carpet(12,10,PaintColor.RED)
+                );
     }
 }
